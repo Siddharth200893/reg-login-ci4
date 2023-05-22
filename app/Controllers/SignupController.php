651eeac75136e2ad6b4526001242998e0e6c7ed5
Siddharth Sharma <sharmasiddharth385@gmail.com>
2023-05-22 17:46:27 +0530

    this is ci4 registration and login process
    -ci4 reg and login

---
+++ app/Controllers/SignupController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+use App\Models\UserModel;
+
+class SignupController extends Controller
+{
+    public function reg_form()
+    {
+        helper(['form']);
+        $data = [];
+        echo view('signup', $data);
+    }
+
+    public function store()
+    {
+        helper(['form']);
+        $rules = [
+            'name'          => 'required',
+            'l_name'         => 'required',
+            'photo'         => 'required',
+            'email'      => 'required',
+            'password'  => 'required',
+            'phone'          => 'required',
+            'message'         => 'required',
+            'created_at'      => 'required',
+            'modified_at'  => 'required',
+        ];
+
+        if ($this->validate($rules)) {
+            $userModel = new UserModel();
+            $data = [
+                'name'     => $this->request->getVar('name'),
+                'l_name'    => $this->request->getVar('l_name'),
+                'photo'    => $this->request->getVar('photo'),
+                'email' => $this->request->getVar('email'),
+                'password'     => $this->request->getVar('password'),
+                'phone'    => $this->request->getVar('phone'),
+                'message' => $this->request->getVar('message'),
+                'created_at'     => $this->request->getVar('created_at'),
+                'modified_at'    => $this->request->getVar('modified_at'),
+
+            ];
+            $userModel->save($data);
+            return redirect()->to('/signin');
+        } else {
+            $data['validation'] = $this->validator;
+            echo view('signup', $data);
+        }
+    }
+}
